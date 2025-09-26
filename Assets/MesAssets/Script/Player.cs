b83d329a9cefb32b16608c11f5a9136ec090dab9
jager08232 <juangamboarozo@gmail.com>
2025-09-26 12:25:23 -0400

    salut

--- Assets/MesAssets/Script/Player.cs
+++ Assets/MesAssets/Script/Player.cs
@@ -4,19 +4,31 @@ public class Player : MonoBehaviour
 {
 
     private Vector3 _direction;
+    [SerializeField] private float _vitesseJoueur = 10f;
+    [SerializeField] private float _vitesseRotationJoueur = 1000f;
+
     private void Update()
     {
-        if (Input.GetKey(KeyCode.RightArrow))
-        {
-            Debug.Log("Droite");
-            _direction = Vector3.right;
-            transform.Translate(_direction);
-        }
-        else if (Input.GetKey(KeyCode.LeftArrow))
-        {
-            Debug.Log("Guache");
-            _direction = Vector3.left;
-            transform.Translate(_direction);
+        MouvementsJoueur();
+    }
+
+    private void MouvementsJoueur()
+    {
+        float directionX = Input.GetAxis("Horizontal");
+        float directionY = Input.GetAxis("Vertical");
+        Debug.Log("Horizontal");
+
+        _direction = new Vector3(directionX, 0f, directionY);
+
+        _direction.Normalize(); //Vecteur be dépasse pas 1
+
+        transform.Translate(_direction * Time.deltaTime * _vitesseJoueur);
+
+        // Rotation dans la direction du movement
+        if(_direction != Vector3.zero)
+        { 
+            Quaternion directionRotation = Quaternion.LookRotation(_direction, Vector3.up);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, directionRotation, _vitesseRotationJoueur * Time.deltaTime);
         }
     }
 }
